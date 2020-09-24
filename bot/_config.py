58a3b2b226d69e360bfe20a3d5c42d15a5f5b1e9
sahasrara62 <nazi1062>
2020-09-25 02:04:53 +0530

    Adding config file
    - Adding the config file for the search, database, and discord bot.
    
    note the credentials are temporially and will be removed in future.

---
+++ bot/_config.py
@@ -0,0 +1,30 @@
+__config = {
+  "search": {
+
+    "tld": "com",
+    "lang": "en",
+    "tbs": "0",
+    "safe": "off",
+    "num": 10,
+    "start": 0,
+    "stop": 5,
+    "pause": 2.0,
+    "country": "",
+    "extra_params": None,
+    "user_agent": None,
+    "verify_ssl": True
+  },
+  "discord_bot": {
+    "token": "NzU4MjYwNDMzMzQxMjUxNjA1.X2sXDw.SYDhB_BZ3YJX3Wbg5yVBODRFu90"
+  },
+  "database": {
+
+    "username":"ozqemvpzhtolvx",
+    "password":"93acb569185cc313cc6c978d75544ce58750fa9cc90c9244705004627472664e",
+    "host":"ec2-52-1-95-247.compute-1.amazonaws.com",
+    "port":5432,
+    "dbname":"d3th6elkr8g4rk"
+  }
+
+  }
+
